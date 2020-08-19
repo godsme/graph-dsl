fc7dea49cb50522d4e6da46e505c9cd8da077225
Darwin Yuan <darwin_yuan@hotmail.com>
2020-08-19 23:44:36 +0800

    added number string

--- tests/ut/test_number_string.cpp
+++ tests/ut/test_number_string.cpp
@@ -13,9 +13,17 @@ namespace {
       static_assert(__CUB_number_string(.0)::value == 0);
       static_assert(__CUB_number_string(0.)::value == 0);
       static_assert(__CUB_number_string(0.123)::value == 0.123);
+      static_assert(__CUB_number_string(-0.123)::value == -0.123);
+      static_assert(__CUB_number_string(-.123)::value == -0.123);
+      static_assert(__CUB_number_string(-14.123)::value == -14.123);
+      static_assert(__CUB_number_string(+0.123)::value == 0.123);
+      static_assert(__CUB_number_string(+14.123)::value == 14.123);
       static_assert(__CUB_number_string(.234)::value == 0.234);
       static_assert(__CUB_number_string(2345.2345678)::value - 2345.2345678 < std::numeric_limits<double>::epsilon());
       static_assert(__CUB_number_string(15.84)::value == 15.84);
       static_assert(__CUB_number_string(26.)::value == 26);
+      static_assert(std::is_base_of_v<GRAPH_DSL_NS::num_s::number_string_signature, __CUB_number_string(123)>);
+      static_assert(std::is_base_of_v<GRAPH_DSL_NS::num_s::number_string_signature, __CUB_number_string(1.23)>);
+      static_assert(std::is_base_of_v<GRAPH_DSL_NS::num_s::open_interval_signature, __CUB_number_string(open_i)>);
    }
 }
